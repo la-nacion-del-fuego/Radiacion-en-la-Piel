312f9a9acdc030232dc3a784c1f92f9d37d28766
Donovan Mosheh Ramirez Trejo <38016639+soyeldono@users.noreply.github.com>
2020-01-05 19:10:43 -0600

    Add files via upload
    visualización de las imagenes

--- rte.py
+++ rte.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python3
 
-#
-#  Radiative Transfer Equation
-#  Copyright (C) 2019 Victor De la Luz
-#                     vdelaluz@enesmorelia.unam.mx
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-
 import math
 import matplotlib.pyplot as plt
 
@@ -30,12 +11,12 @@ def k(L):
 
 #optical depth (adimensional)
 def tau(dx,x,wl,L):
-    return (dx/2.0)*(k(L) + k(L))
+    return (dx/2.0)*(k(L-dx) + k(L))
 
 N = 100
-Ns = [100,40,1000,200]
-Names = ["Talon del pie","Parpado","Abdomen Personas Obesas","Abdomen Personas Delgadas"]
-colorsGraphs = ["Green","Blue","Purple","Red"]
+Ns = [100,40,1000,200,5]
+Names = ["Talon del pie","Parpado","Abdomen Personas Obesas","Abdomen Personas Delgadas","Escroto"]
+colorsGraphs = ["Green","Blue","Purple","Red","Brown"]
 I0 = 250000 #[erg/cm2 sec cm ster]
 nu = 1e8 #Hz
 dx = 0.1   #[mm]
@@ -59,18 +40,13 @@ for n,j in zip(Names,Ns):
     savesY.append(Y)
     savesNames.append(n)
 
-fig,ax = plt.subplots(2,2)
-fig.suptitle("Absorcion de la luz en diferentes partes del cuerpo")
-fig.set_size_inches(13,10,forward=True)
 iterator = 0
-for y in range(2):
-    for x in range(2):
-        ax[y][x].plot(savesX[x+y],savesY[x+y],colorsGraphs[iterator])
-        ax[y][x].set_title(Names[iterator])
-        iterator += 1
-
-for ax in ax.flat:
-    ax.set(xlabel='Densidad de la piel en mm', ylabel='Intensidad Especifica')
-
-plt.savefig("Ejemplo.jpg")
-plt.show()
+for x,y in zip(savesX,savesY):
+    plt.figure(figsize=(8,5))
+    plt.plot(x,y,colorsGraphs[iterator])
+    plt.title(Names[iterator])
+    plt.xlabel("Densidad de la piel en mm")
+    plt.ylabel("Intensidad Especifica")
+    plt.savefig("Ejemplo "+ Names[iterator] + ".jpg")
+    iterator += 1
+    plt.show()
